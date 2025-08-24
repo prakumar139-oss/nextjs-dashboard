48f2d7055b5891cdfb34dff4e4aa45d68dbc5d7f
Praveen Kumar <prakumar139@WKMZT02D3CF2.global.publicisgroupe.net>
2025-08-24 18:01:18 +0530

    updated file

--- global.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
