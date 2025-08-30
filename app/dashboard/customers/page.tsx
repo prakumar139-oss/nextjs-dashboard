6ebaa77616284f3c52a4f4472af298e5fb11461b
Praveen Kumar <prakumar139@WKMZT02D3CF2.global.publicisgroupe.net>
2025-08-30 18:15:22 +0530

    customer integration for dashboard

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-  return <p>Customer Page</p>;
+import React, { Suspense } from "react";
+import Table from "@/app/ui/customers/table";
+import { CustomerTableSkeleton } from "@/app/ui/skeletons";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <Suspense fallback={<CustomerTableSkeleton />}>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
